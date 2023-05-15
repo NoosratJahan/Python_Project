ba8996f83403c0077b8e68d2b8ede7dcdb26279e
NoosratJahan <131634101+NoosratJahan@users.noreply.github.com>
2023-05-15 19:55:51 +0600

    Update urls.py

--- mysite/crud/urls.py
+++ mysite/crud/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name=''),
     path('form/', views.form, name='form'),
-    path('<int:id>/', views.form, name='update'),
+    path('update/<int:id>/', views.update, name='update'),
     path('show/', views.show, name='show'),
-    path('delete/<int:id>/', views.delete, name='delete')
+    path('delete/<int:id>/', views.delete, name='delete'),
        
 ]
